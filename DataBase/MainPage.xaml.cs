51e2d6d9c7877c2c6b34bd4802d185f7b56f128a
MasAlecs <alecs2018,ru@mail.ru>
2024-05-06 22:46:21 +0300

    Создан репозиторий, существующие две страницы с навигацией с одной на другую

---
+++ DataBase/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBase
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ContactGrid.ItemsSource = ContactEntities.GetContext().Telephone.ToList();
+        }
+
+        private void Button_Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AddEditPage());
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
